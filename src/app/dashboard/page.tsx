acc82b57be3c10e7eabc92cebd37294fadf97fa6
alanicio <alanicio98@outlook.com>
2025-02-24 07:09:19 -0600

    layout changed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import MainChart from "@/components/dashboard-layout/main-chart";
 import SecondaryChart from "@/components/dashboard-layout/secondary-chart";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import React from "react";
 
 const CardTest = () => (
@@ -10,26 +11,22 @@ const CardTest = () => (
 );
 
 const DashboardPage = () => {
+   const gapClassName = "gap-8";
+   const rowClassName = "grid grid-cols-6";
    return (
-      <div className="grid grid-cols-1 gap-4">
-         <div className="grid grid-cols-7 gap-4">
+      <div className={cn("grid grid-cols-1 gap", gapClassName)}>
+         <div className={cn(rowClassName, gapClassName)}>
             <MainChart className="col-span-4" />
             <Card className="p-4">
                <CardTest />
             </Card>
-            <Card className="p-4">
-               <CardTest />
-            </Card>
-            <Card className="p-4">
+            <Card className="p-3">
                <CardTest />
             </Card>
          </div>
-         <div className="grid grid-cols-7 gap-4">
+         <div className={cn(rowClassName, gapClassName)}>
             <SecondaryChart className="col-span-2" />
             <Card className="p-4 col-span-2">
-               <CardTest />
-            </Card>
-            <Card className="p-4">
                <CardTest />
             </Card>
             <Card className="p-4 col-span-2">
