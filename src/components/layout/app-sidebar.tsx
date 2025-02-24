ac0b84615675adf84037f14e8e176dfefbb9e4f8
alanicio <alanicio98@outlook.com>
2025-02-23 21:26:10 -0600

    test menu items added to sidebar

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,12 +1,36 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } from "@/components/ui/sidebar";
+import {
+   Sidebar,
+   SidebarContent,
+   SidebarFooter,
+   SidebarGroup,
+   SidebarHeader,
+   SidebarMenu,
+   SidebarMenuButton,
+   SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { menuItems } from "./sidebar-menus";
 
 export function AppSidebar() {
    return (
       <Sidebar>
-         <SidebarHeader />
+         <SidebarHeader>
+            <h4>Content</h4>
+         </SidebarHeader>
          <SidebarContent>
-            <SidebarGroup />
-            <SidebarGroup />
+            <SidebarGroup>
+               <SidebarMenu>
+                  {menuItems.map((menuItem) => (
+                     <SidebarMenuItem key={menuItem.title}>
+                        <SidebarMenuButton asChild>
+                           <a href={menuItem.url}>
+                              <menuItem.icon />
+                              <span>{menuItem.title}</span>
+                           </a>
+                        </SidebarMenuButton>
+                     </SidebarMenuItem>
+                  ))}
+               </SidebarMenu>
+            </SidebarGroup>
          </SidebarContent>
          <SidebarFooter />
       </Sidebar>
