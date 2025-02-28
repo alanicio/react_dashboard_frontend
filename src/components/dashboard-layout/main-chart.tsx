73c15d03edeb656a7c2bcc1a0b656415352fd137
alanicio <alanicio98@outlook.com>
2025-02-27 23:34:51 -0600

    big data dummy added

--- src/components/dashboard-layout/main-chart.tsx
+++ src/components/dashboard-layout/main-chart.tsx
@@ -13,7 +13,8 @@ interface MainChart {
 }
 
 const MainChart = ({ className, chartElements, data }: MainChart) => {
-   const [displayedRange, setDisplayedRange] = useState([0, data.length < 5 ? data.length : 5]);
+   const [displayedRange, setDisplayedRange] = useState([0, data.length < 6 ? data.length - 1 : 5]);
+   const dataToDisplay = data.slice(displayedRange[0], displayedRange[1] + 1);
    return (
       <Card className={className}>
          <CardHeader>
@@ -22,10 +23,16 @@ const MainChart = ({ className, chartElements, data }: MainChart) => {
          </CardHeader>
          <CardContent className="w-full h-[38vh] flex flex-col justify-between">
             <div className="h-[90%]">
-               <DisplayAreaChart chartElements={chartElements} data={data.slice(...displayedRange)} />
+               <DisplayAreaChart chartElements={chartElements} data={dataToDisplay} />
             </div>
             <div className="w-[50%] px-[60px] pb-2.5">
-               <Slider value={displayedRange} onValueChange={setDisplayedRange} max={data.length} step={1} />
+               <Slider
+                  value={displayedRange}
+                  onValueChange={setDisplayedRange}
+                  max={data.length - 1}
+                  step={1}
+                  minStepsBetweenThumbs={1}
+               />
             </div>
          </CardContent>
       </Card>
--- src/mock-data/area-data.ts
+++ src/mock-data/area-data.ts
@@ -4,7 +4,7 @@ export const chartElements = [
     { key: "metric3", stroke: "#0000ff", fill: "#ccccff" },
  ];
  
- export const data = [
+ export const dataSmall = [
     {
        name: "Page A",
        metric1: 4000,
@@ -48,3 +48,32 @@ export const chartElements = [
        metric3: 2100,
     },
  ];
+
+ export const data= [
+   {"name": "Page A", "metric1": 18507, "metric2": 12458, "metric3": 19792},
+   {"name": "Page B", "metric1": 11619, "metric2": 8607, "metric3": 3837},
+   {"name": "Page C", "metric1": 14803, "metric2": 10153, "metric3": 10270},
+   {"name": "Page D", "metric1": 10623, "metric2": 18604, "metric3": 13574},
+   {"name": "Page E", "metric1": 12589, "metric2": 2273, "metric3": 17517},
+   {"name": "Page F", "metric1": 1516, "metric2": 5038, "metric3": 13320},
+   {"name": "Page G", "metric1": 13609, "metric2": 10805, "metric3": 12406},
+   {"name": "Page H", "metric1": 13156, "metric2": 15912, "metric3": 2777},
+   {"name": "Page I", "metric1": 11753, "metric2": 15663, "metric3": 17850},
+   {"name": "Page J", "metric1": 4191, "metric2": 2526, "metric3": 12662},
+   {"name": "Page K", "metric1": 14136, "metric2": 5928, "metric3": 9033},
+   {"name": "Page L", "metric1": 17787, "metric2": 14985, "metric3": 13238},
+   {"name": "Page M", "metric1": 6802, "metric2": 16394, "metric3": 8833},
+   {"name": "Page N", "metric1": 10523, "metric2": 4505, "metric3": 12273},
+   {"name": "Page O", "metric1": 19204, "metric2": 7322, "metric3": 8292},
+   {"name": "Page P", "metric1": 9411, "metric2": 16527, "metric3": 14890},
+   {"name": "Page Q", "metric1": 18564, "metric2": 4969, "metric3": 9093},
+   {"name": "Page R", "metric1": 13585, "metric2": 13106, "metric3": 17064},
+   {"name": "Page S", "metric1": 15950, "metric2": 11876, "metric3": 5998},
+   {"name": "Page T", "metric1": 5673, "metric2": 17885, "metric3": 15274},
+   {"name": "Page U", "metric1": 1187, "metric2": 13665, "metric3": 9923},
+   {"name": "Page V", "metric1": 18808, "metric2": 7156, "metric3": 9266},
+   {"name": "Page W", "metric1": 13597, "metric2": 3372, "metric3": 3180},
+   {"name": "Page X", "metric1": 17768, "metric2": 8314, "metric3": 9762},
+   {"name": "Page Y", "metric1": 15946, "metric2": 10251, "metric3": 11009},
+   {"name": "Page Z", "metric1": 19570, "metric2": 2822, "metric3": 17477}
+]
