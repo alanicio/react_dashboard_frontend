3b3bddc31ea8a9f15d93d9f368cd6e30b1e9c4a2
alanicio <alanicio98@outlook.com>
2025-03-07 02:50:59 -0600

    pie size changed

--- src/components/charts/pie-chart.tsx
+++ src/components/charts/pie-chart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 
 const data = [
    { name: "Group A", value: 400 },
@@ -28,14 +28,14 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 const DisplayPieChart = () => {
    return (
       <ResponsiveContainer width="100%" height="100%">
-         <PieChart width={400} height={400}>
+         <PieChart width={250} height={250}>
             <Pie
                data={data}
                cx="50%"
                cy="50%"
                labelLine={false}
                label={renderCustomizedLabel}
-               outerRadius={80}
+               outerRadius={100}
                fill="#8884d8"
                dataKey="value"
             >
@@ -43,6 +43,8 @@ const DisplayPieChart = () => {
                   <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                ))}
             </Pie>
+            <Legend layout="vertical" verticalAlign="middle" align="right" />
+            <Tooltip />
          </PieChart>
       </ResponsiveContainer>
    );
