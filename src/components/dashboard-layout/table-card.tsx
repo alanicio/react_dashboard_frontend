7801b0d6f8070934143eb5125caf7235c28483c0
alanicio <alanicio98@outlook.com>
2025-03-23 01:19:02 -0600

    working in table card

---
+++ src/components/dashboard-layout/table-card.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "@/components/ui/card";
+
+const TableCard = () => {
+   return <Card className="p-4 col-span-2">TableCard</Card>;
+};
+
+export default TableCard;
