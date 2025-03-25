dd4f097472c3a9a9da38cbedaeb196a7c69ecf92
alanicio <alanicio98@outlook.com>
2025-03-24 21:09:38 -0600

    types fixed

--- src/components/dashboard-layout/table-card.tsx
+++ src/components/dashboard-layout/table-card.tsx
@@ -2,15 +2,21 @@
 
 import React, { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AllCommunityModule, ColDef, ModuleRegistry } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 
-// Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
+
+interface RowData {
+   make: string;
+   model: string;
+   price: number;
+   electric: boolean;
+}
 
 const GridExample = () => {
    // Row Data: The data to be displayed.
-   const [rowData, setRowData] = useState([
+   const [rowData] = useState<RowData[]>([
       { make: "Tesla", model: "Model Y", price: 64950, electric: true },
       { make: "Ford", model: "F-Series", price: 33850, electric: false },
       { make: "Toyota", model: "Corolla", price: 29600, electric: false },
@@ -21,8 +27,7 @@ const GridExample = () => {
       { make: "Volkswagen", model: "Jetta", price: 33700, electric: false },
    ]);
 
-   // Column Definitions: Defines & controls grid columns.
-   const [colDefs, setColDefs] = useState([
+   const [colDefs] = useState<ColDef<RowData>[]>([
       { field: "make" },
       { field: "model" },
       { field: "price" },
