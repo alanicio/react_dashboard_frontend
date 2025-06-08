688ad730e572b9eb6590279581839e5e4207ef17
alanicio <alanicio98@outlook.com>
2025-06-08 01:01:04 -0600

    posible fix, requires more testing

--- src/components/layout/page-content.tsx
+++ src/components/layout/page-content.tsx
@@ -4,7 +4,7 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 const PageContent = ({ children }: { children: React.ReactNode }) => {
    return (
       <div className="flex">
-         <ScrollArea type="always" className="h-[calc(100vh-28px)] w-1 flex-1 overflow-x-auto">
+         <ScrollArea type="always">
             <div className="px-4 sm:px-12 py-6">{children}</div>
             <ScrollBar orientation="horizontal" className="w-full" />
          </ScrollArea>
