20a9b08844545616164d23a1dbc19d790275f998
alanicio <alanicio98@outlook.com>
2025-06-30 01:42:56 -0600

    pagination improved

--- src/components/table/pagination.tsx
+++ src/components/table/pagination.tsx
@@ -40,6 +40,14 @@ const Pagination = ({
       <Button variant="outline" size="sm" onClick={() => previousPage()} disabled={!getCanPreviousPage()}>
          Previous
       </Button>
+      {pageCount > 10 && currentPageIndex > 4 && (
+         <PaginationEllipsis setPageIndex={setPageIndex} currentPageIndex={currentPageIndex} pageCount={pageCount} />
+      )}
+      {pageCount > 10 && currentPageIndex === 4 && (
+         <Button variant="outline" size="sm" onClick={() => setPageIndex(0)}>
+            1
+         </Button>
+      )}
       {getVisiblePages({ currentPageIndex, pageCount }).map((index) => (
          <Button
             key={index}
@@ -50,52 +58,14 @@ const Pagination = ({
             {index + 1}
          </Button>
       ))}
-      {/* {Array.from({ length: pageCount > 10 ? 5 : pageCount }, (_, index) => (
-         <Button
-            key={index}
-            size="sm"
-            variant={index === currentPageIndex ? "default" : "outline"}
-            onClick={() => setPageIndex(index)}
-         >
-            {index + 1}
-         </Button>
-      ))}
-      {pageCount > 10 && (
-         <Popover>
-            <PopoverTrigger asChild>
-               <Button key="custom number" variant="outline" size="sm">
-                  ...
-               </Button>
-            </PopoverTrigger>
-            <PopoverContent>
-               <form
-                  onSubmit={(e) => {
-                     e.preventDefault();
-                     setPageIndex(parseInt(e.currentTarget.customPage.value) - 1);
-                  }}
-               >
-                  <Input type="number" name="customPage" min={1} max={pageCount} defaultValue={currentPageIndex + 1} />
-               </form>
-            </PopoverContent>
-         </Popover>
+      {pageCount > 10 && currentPageIndex < pageCount - 5 && (
+         <PaginationEllipsis setPageIndex={setPageIndex} currentPageIndex={currentPageIndex} pageCount={pageCount} />
       )}
-      {pageCount > 10 && (
-         <>
-            {Array.from({ length: 3 }, (_, index) => {
-               const pageNumber = pageCount - 3 + index;
-               return (
-                  <Button
-                     key={pageNumber}
-                     variant={pageNumber === currentPageIndex ? "default" : "outline"}
-                     size="sm"
-                     onClick={() => setPageIndex(pageNumber)}
-                  >
-                     {pageNumber + 1}
-                  </Button>
-               );
-            })}
-         </>
-      )} */}
+      {pageCount > 10 && currentPageIndex === pageCount - 5 && (
+         <Button variant="outline" size="sm" onClick={() => setPageIndex(pageCount - 1)}>
+            {pageCount}
+         </Button>
+      )}
       <Button variant="outline" size="sm" onClick={() => nextPage()} disabled={!getCanNextPage()}>
          Next
       </Button>
