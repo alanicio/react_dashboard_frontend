f220932122a4b461feef12384164189ddadec559
alanicio <alanicio98@outlook.com>
2025-06-30 02:27:55 -0600

    page header created

--- src/app/users/_components/users-page.tsx
+++ src/app/users/_components/users-page.tsx
@@ -4,13 +4,19 @@ import { DataTable } from "@/components/table/data-table";
 import { User } from "@/types/user";
 import React from "react";
 import { createColumns } from "./columns";
+import PageHeader from "@/components/layout/page-header";
 
 interface UsersPageProps {
    intialUsers: User[];
 }
 const UsersPage = ({ intialUsers }: UsersPageProps) => {
    const columns = createColumns();
-   return <DataTable columns={columns} data={intialUsers} />;
+   return (
+      <>
+         <PageHeader title="Users" subtitle="List of users" />
+         <DataTable columns={columns} data={intialUsers} />;
+      </>
+   );
 };
 
 export default UsersPage;
